be6f6cc0948364be759e04c0020c703890761157
Alexandre Ruban <alexandre@hey.com>
2024-10-16 15:49:44 +0200

    Add tests for categories controller
    Co-authored-by: jeannetian <tian.jeanne@gmail.com>

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:alex)
+
+    sign_in @user
+  end
+
+  test "#index" do
+    get categories_path
+
+    assert_response :success
+  end
+
+  test "#new" do
+    get new_category_path
+
+    assert_response :success
+  end
+
+  test "#create" do
+    assert_difference("@user.categories.count", 1) do
+      post categories_path, params: { category: { name: "New Category" } }
+    end
+
+    assert_redirected_to categories_path
+  end
+
+  test "#edit" do
+    get edit_category_path(categories(:food))
+
+    assert_response :success
+  end
+
+  test "#update" do
+    assert_changes("categories(:food).reload.name", to: "Food!") do
+      patch category_path(categories(:food)), params: { category: { name: "Food!" } }
+    end
+
+    assert_redirected_to categories_path
+  end
+
+  test "#destroy" do
+    assert_difference("@user.categories.count", -1) do
+      delete category_path(categories(:food))
+    end
+
+    assert_redirected_to categories_path
+  end
 end
