3ba21d7387d4037b7e9b4400891874284897107a
Alexandre Ruban <alexandre@hey.com>
2024-10-16 16:17:40 +0200

    Create the Account model
    Co-authored-by: jeannetian <tian.jeanne@gmail.com>

---
+++ db/migrate/20241016141428_create_accounts.rb
@@ -0,0 +1,11 @@
+class CreateAccounts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name, null: false
+      t.decimal :balance, precision: 15, scale: 2, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
